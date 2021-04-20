6d16dc13b62d9f3507fccf09edee41fc299f9e3c
jasminsumaiya <sumaiya@provility.com>
2021-04-20 10:36:31 +0530

    implemented form validation in login component

--- day2/frontend/src/app/login/login.component.ts
+++ day2/frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,23 +11,32 @@ export class LoginComponent implements OnInit {
 
   username: string = '';
   password: string = '';
+  submitted = false;
+  projectCreationForm: FormGroup;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.projectCreationForm = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
-  onClickLogin() {
+  get controls() { return this.projectCreationForm.controls; }
 
-    let loginData = {
-      username: this.username,
-      password: this.password
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.projectCreationForm.invalid) {
+      return;
     }
+
+    let loginData = this.projectCreationForm.value;
     let token: string = this.getToken(loginData);
     localStorage.setItem('token', token);
 
     this.navigateToProjectList();
-
   }
 
   getToken(loginData): string {
