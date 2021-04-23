600d338b0921f0c231a240e4063c3e2a8d3641e2
Buhari <apple@Apple.local>
2021-04-23 16:41:47 +0530

    post with multiple comments

--- day2/frontend/src/app/post/post.component.ts
+++ day2/frontend/src/app/post/post.component.ts
@@ -8,10 +8,13 @@ import { PostService } from "./post.service";
   styleUrls: ["./post.component.css"],
 })
 export class PostComponent implements OnInit {
+
   post;
-  selectedPost;
-  comments;
-  showComments: boolean = false;
+  // showComments ;
+  selectedPost: any = [];
+  comments:any = {};
+
+  // showComments: boolean = false; 
   // FOR SPINNER 
   spinnerLoading: boolean = true;
  
@@ -21,14 +24,15 @@ export class PostComponent implements OnInit {
   }
 
   ngOnInit() {
-    const postData = this.postService.fetchPost();
-    postData.subscribe((post) => {
+      const postData = this.postService.fetchPost();
+      postData.subscribe((post) => {
       console.log(post);
       this.spinnerLoading = false;
       this.post = post;
     });
   }
 
+  /* WHEN CLICKING THE POST SHOW THAT PARTICULAR POST COMMENT (SINGLE POST WITH SINGLE CMT)
   onClickShowPostComments(post) {
     console.log("Im clicked");
     this.selectedPost = post;
@@ -36,9 +40,28 @@ export class PostComponent implements OnInit {
     commentsData.subscribe((comments) => {
       console.log(comments);
       this.comments = comments;
-      this.showComments = true;
-     
     });
-  }
+  }*/
 
+
+
+
+  // WHEN CLICKING THE POST SHOW THAT PARTICULAR POST COMMENT (MULTIPLE POST MULTIPLE CMT)
+
+  onClickShowPostComments(post) {
+    console.log("I'm Got Clicked !")
+    if(!this.selectedPost.includes(post.id)) {
+      this.selectedPost.push(post.id);
+      const commentsData = this.postService.fetchPostByComments(post.id);
+      commentsData.subscribe((comment) => {
+        this.comments[post.id] = comment;
+      })
+    }
+    else {
+      this.selectedPost = this.selectedPost.filter(id => id !== post.id);
+      this.comments[post.id] = [];
+    }
+  }
 }
+
+
